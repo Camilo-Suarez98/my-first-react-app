9a74804c24be609aefb190b8ce49c21117f39327
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-06-26 14:42:43 -0500

    Change at the position of the image

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import scarecrow from '../assets/scarecrow.png';
 
 export const Content = () => {
   return (
     <main className='body-page'>
       <div className="image-page">
-        <img className='image' src={scarecrow} alt="Scarecrow" />
+        <img className='image' src='../../public/img/scarecrow.png' alt="Scarecrow" />
       </div>
       <div className="text">
         <h1>I have bad news for you</h1>
